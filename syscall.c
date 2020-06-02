d9e57242a1373428b17af4ddd1d656c37d4c9e8a
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-06-02 22:55:10 +0430

    c4

--- syscall.c
+++ syscall.c
@@ -103,7 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int SYS_getpinfo(void); //function prototype
+extern int SYS_getpinfo(void); /*function prototype*/
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,7 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_getpinfo] SYS_getpinfo, //add pointer to the systemcall 
+[SYS_getpinfo] SYS_getpinfo, /*add pointer to the systemcall */
 };
 
 void
