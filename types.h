3e2e023ed8cf79a20303107a21f4c315482b1126
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-06-04 16:56:06 +0430

    add new struct

--- types.h
+++ types.h
@@ -2,3 +2,9 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+/*the given struct*/
+struct proc_info
+{
+  int pid;
+  int memsize;
+}
