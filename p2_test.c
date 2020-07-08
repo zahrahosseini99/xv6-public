b52f7c7bd15228707109be9de9e44a7a9c97a0f0
zahrahosseini99 <hosseini99.zahra@gmail.com>
2020-07-09 00:29:46 +0430

    adding new syscall

--- p2_test.c
+++ p2_test.c
@@ -12,51 +12,38 @@ int main(void)
 		int  _pid1 = fork();
 		if( _pid1 == 0)
 		{
-			int  _pid2 = fork();
-			if( _pid2 == 0)
-			{
-				set_priority(30);
+
+				set_priority(38);
+				sleep(90);
+				int temp = 0;
+				for (int i = 0; i < 1000000000; i++)
+		    {
+		     temp++;
+		    }
+				exit();
+			}
+			else{
+				set_priority(78);
 				sleep(100);
 				int temp = 0;
-				while(temp<1000000000)
-					temp++;
-				exit();
-			}
-			else
-			{
-				set_priority(40);
-
-		   	sleep(90);
-
-				int temp = 0;
-				while(temp<1000000000)
-					temp++;
+				for (int i = 0; i < 1000000000; i++)
+				{
+				 temp++;
+				}
 				wait();
 				exit();
-
 			}
+
 		}
-		else{
-			set_priority(70);
+		else
+		{
 			sleep(100);
-			int temp = 0;
-			for (int i = 0; i < 1000000000; i++)
-	    {
-	     temp++;
-	    }
+			printf(1,"waiting for child of this proc \n");
+			ps();
 			wait();
 			exit();
-		}
-	}
-	else
-	{
-		sleep(100);
-		printf(1,"waiting for child of this proc \n");
-		ps();
-		wait();
-	  exit();
 
-	}
+		}
 
 	return 0;
-}
+	}
